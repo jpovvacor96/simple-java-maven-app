c7897de67e14b258962f47526ff4c3e7eb423581
USERSAD\jpalomio <jpalomio@emeal.nttdata.com>
2024-02-16 16:16:44 +0100

    Cambio en el nombre de usuario, el repositorio y el correo electrónico

--- DSL/javamavenDSL.groovy
+++ DSL/javamavenDSL.groovy
@@ -1,9 +1,9 @@
 job('Java Maven App DSL') {
     description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/simple-java-maven-app.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/jpovvacor96/simple-java-maven-app.git', 'master') { node ->
+            node / gitConfigName('jpovvacor96')
+            node / gitConfigEmail('jpovvacor96@gmail.com')
         }
     }
     steps {
